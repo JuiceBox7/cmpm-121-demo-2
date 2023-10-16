85562c19bef6ea9b45789a578d922041b75c38b2
JuiceBox7 <juiceinbox7@gmail.com>
2023-10-15 23:47:29 -0700

    Step 4 complete

--- src/main.ts
+++ src/main.ts
@@ -30,25 +30,15 @@ ctx!.fillRect(zero, zero, maxCanvasWidth, maxCanvasHeight);
 
 app.append(document.createElement("br"));
 
-const clearBtn = document.createElement("button");
-clearBtn.innerHTML = "Clear";
-app.append(clearBtn);
-
-clearBtn.addEventListener("click", (e) => {
-  console.log(e);
-  strokes.splice(zero, strokes.length);
-  canvas.dispatchEvent(drawingChanged);
-});
-
-const cursor = { active: false, x: zero, y: zero };
-
 interface Pair {
   x: number;
   y: number;
 }
+const cursor = { active: false, x: zero, y: zero };
 
 const strokes: Pair[][] = [];
 let currStroke: Pair[] = [];
+const redoStrokes: Pair[][] = [];
 
 const drawingChanged = new CustomEvent("drawing-changed");
 
@@ -57,6 +47,7 @@ canvas.addEventListener("mousedown", (e) => {
   cursor.active = true;
   currStroke = [];
   strokes.push(currStroke);
+  redoStrokes.splice(zero, redoStrokes.length);
   currStroke.push({ x: cursor.x, y: cursor.y });
   canvas.dispatchEvent(drawingChanged);
 });
@@ -84,6 +75,40 @@ canvas.addEventListener("mouseup", (e) => {
 canvas.addEventListener("drawing-changed", (e) => {
   console.log(e);
   redraw();
+});
+
+const clearBtn = document.createElement("button");
+clearBtn.innerHTML = "Clear";
+app.append(clearBtn);
+
+clearBtn.addEventListener("click", (e) => {
+  console.log(e);
+  strokes.splice(zero, strokes.length);
+  canvas.dispatchEvent(drawingChanged);
+});
+
+const undoBtn = document.createElement("button");
+undoBtn.innerHTML = "Undo";
+app.append(undoBtn);
+
+undoBtn.addEventListener("click", (e) => {
+  console.log(e);
+  if (strokes.length > zero) {
+    redoStrokes.push(strokes.pop()!);
+    canvas.dispatchEvent(drawingChanged);
+  }
+});
+
+const redoBtn = document.createElement("button");
+redoBtn.innerHTML = "Redo";
+app.append(redoBtn);
+
+redoBtn.addEventListener("click", (e) => {
+  console.log(e);
+  if (redoStrokes.length > zero) {
+    strokes.push(redoStrokes.pop()!);
+    canvas.dispatchEvent(drawingChanged);
+  }
 });
 
 function setOffset(e: MouseEvent): void {
