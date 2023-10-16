b339563a8cdac6e9ff867f09106e8b3d14b53013
JuiceBox7 <juiceinbox7@gmail.com>
2023-10-15 21:47:15 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,16 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const appName = "Canvas";
+
+const appTitle = document.createElement("h1");
+appTitle.innerHTML = appName;
+app.append(appTitle);
+
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
+app.append(canvas);
+
+ctx!.fillStyle = "white";
+ctx!.fillRect(20, 0, 256, 256);
